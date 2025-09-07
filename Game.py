6d9ba75a6db25b53e7f34591032d216d21b3270b
Stephanie Garcia <stephanie.garcia33@upr.edu>
2025-09-07 19:39:00 -0400

    Task 6 completed

--- Game.py
+++ Game.py
@@ -25,6 +25,8 @@ def ball_movement():
             score = score + 1  # Increase player score
             ball_speed_y *= -1  # Reverse ball's vertical direction
             # TODO Task 6: Add sound effects HERE
+            sound_1 = pygame.mixer.Sound('sound.wav')
+            sound_1.play()
 
     # Ball collision with top boundary
     if ball.top <= 0:
